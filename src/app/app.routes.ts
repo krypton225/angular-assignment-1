56228c3fb50e56f9527bbd3577f012b890304650
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-18 23:32:56 +0300

    Add routing configuration for app with paths for intro, about, portfolio, and contact sections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "intro",
+    pathMatch: "full",
+  },
+  {
+    path: `intro`,
+    loadComponent: () => import(`./intro/intro.component`).then(m => m.IntroComponent),
+    title: `Intro section`,
+  },
+  {
+    path: `about`,
+    loadComponent: () => import(`./about/about.component`).then(m => m.AboutComponent),
+    title: `About section`,
+  },
+  {
+    path: `portfolio`,
+    loadComponent: () => import(`./portfolio/portfolio.component`).then(m => m.PortfolioComponent),
+    title: `Portfolio section`,
+  },
+  {
+    path: `contact`,
+    loadComponent: () => import(`./contact/contact.component`).then(m => m.ContactComponent),
+    title: `Contact section`,
+  },
+];
