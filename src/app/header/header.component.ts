06e8add1700edb003ba0579750022352345d980f
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-18 22:49:50 +0300

    Implement menu toggle functionality & enhance link interactions in header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @ViewChild("togglerList") togglerList !: ElementRef<HTMLUListElement>;
 
+  toggleMenuHandler(): void {
+    this.togglerList.nativeElement.classList.toggle("js-show");
+  }
+
+  closeMenu(event: Event): void {
+    const clickedElement = event.target as HTMLElement;
+
+    if (clickedElement.tagName.toLowerCase() === 'a') {
+      this.togglerList.nativeElement.classList.remove("js-show");
+    }
+  }
 }
