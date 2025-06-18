dd3e89f76dd9cc00de6900087ff85ee75b0f792f
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-18 23:26:23 +0300

    Enhance header component with scroll behavior & dynamic styling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -11,7 +11,21 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @ViewChild("navbarHeader") navbarHeader !: ElementRef<HTMLElement>;
   @ViewChild("togglerList") togglerList !: ElementRef<HTMLUListElement>;
+
+  isScrolled: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const MIN_SCROLL_TOP = 10;
+
+    if (window.scrollY > MIN_SCROLL_TOP) {
+      this.navbarHeader.nativeElement.classList.add("js-scrollable");
+    } else {
+      this.navbarHeader.nativeElement.classList.remove("js-scrollable");
+    }
+  }
 
   toggleMenuHandler(): void {
     this.togglerList.nativeElement.classList.toggle("js-show");
