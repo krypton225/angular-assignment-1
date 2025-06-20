2e81069d5d374bd39666e9f507d9f29922f6be0b
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-06-20 10:57:16 +0300

    Add portfolio modal functionality with image display & interaction

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { TitleComponent } from "../title/title.component";
 import { IPortfolioCard } from '../../interfaces/portfolio-card.interface';
+import { ModalComponent } from "../modal/modal.component";
 
 @Component({
   selector: 'app-portfolio',
-  imports: [TitleComponent],
+  imports: [
+    TitleComponent,
+    ModalComponent
+  ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
+  isModalOpen: boolean = false;
+  selectedImage: string = ``;
+  selectedImageAlt: string = ``;
+
   cards: IPortfolioCard[] = [
     {
       name: 'Project 3',
@@ -35,4 +43,22 @@ export class PortfolioComponent {
       imagePath: 'images/portfolio/portfolio-1.png',
     },
   ];
+
+  openModal(event: Event): void {
+    const cardElement = event.target as Element;
+    const parentElement = cardElement.closest(`.portfolio-card`) as HTMLDivElement;
+    const imgElement = parentElement.querySelector(`.portfolio-card__img img`) as HTMLImageElement;
+
+    if (imgElement) {
+      this.selectedImage = imgElement.src;
+      this.selectedImageAlt = imgElement.alt || `Portfolio image`;
+
+      this.isModalOpen = true;
+    }
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+    this.selectedImage = ``;
+  }
 }
